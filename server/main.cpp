22e5d28607366a84c1a491e272e73efd1b2e44e3
Diego Fidalgo <diego.fidalgo2@gmail.com>
2021-09-20 11:19:54 -0300

    Adicionado erro 404 para requisições não GET

--- server/main.cpp
+++ server/main.cpp
@@ -17,19 +17,24 @@ int main(int argc, char const * argv[]) {
         return 1;
     }
 
-    const std::string dir = argv[3];
+    // Criação do caminho do diretório
+    const fs::path path(argv[3]);
 
-    const fs::path path(dir);
-
+    // Verificação da existência do diretório
     if (!fs::is_directory(path)) {
         cout << argv[3] << ": is not a directory" << endl;
         return 1;
     }
 
+    // Criação do servidor com a função de processamento de requisição
     auto server = HttpServer([&path](HttpRequest& req, HttpResponse& res) {
+        // Criação do caminho completo para o arquivo;
         auto filepath = path / fs::path(req.path());
 
-        if (!fs::is_regular_file(filepath)) {
+        // Verificação de erro 404
+        // Nesse servidor específico, as únicas rotas existentes são GETs
+        // para caminhos válidos de arquivo.
+        if (req.method() != "GET" || !fs::is_regular_file(filepath)) {
             res
                 << "HTTP/" << req.version() << " 404 Not Found\r\n"
                 << "Content-Type: text/plain\r\n"
@@ -39,32 +44,42 @@ int main(int argc, char const * argv[]) {
             return;
         }
 
+        // Determinação do tamanho do arquivo para preencher o header
+        // Content-Length
         auto size = fs::file_size(filepath);
 
+        // Criação do fd do arquivo para transmití-lo para a conexão TCP
         int fd = open(filepath.c_str(), O_RDONLY);
 
+        // Envio dos headers de resposta
         res
             << "HTTP/" << req.version() << " 200 Ok\r\n"
             << "Content-length: " << size << "\r\n"
             << "\r\n";
 
+        // Leitura do arquivo
         char buffer[4096];
         int n_bytes;
 
         while ((n_bytes = read(fd, buffer, 4096)) > 0) {
+            // Transmissão do arquivo
             res << std::string(buffer, buffer + n_bytes);
         }
     });
 
+    // Criação da thread de escuta do servidor
     auto * server_thread = server.listen(argv[1], atoi(argv[2]));
 
+    // Checagem de sucesso na abertura do servidor
     if (!server_thread) {
         cout << "Error when trying to listen on " << argv[1] << ':' << argv[2] << endl;
         return 2;
     }
 
+    // Mensagem informativa de inicialização
     std::cout << "Server started at " << argv[1] << ':' << argv[2] << endl;
 
+    // Acoplamento da thread do servidor à linha de execução principal
     server_thread->join();
 
     return 0;
